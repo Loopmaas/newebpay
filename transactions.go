e41aa06979f6dd135663f660c2fd71b1e8712d03
vincent <vincent@loopmaas.com>
2025-06-27 14:13:44 +0800

    調整錯誤訊息回傳內容

--- transactions.go
+++ transactions.go
@@ -89,7 +89,8 @@ func (a Api) CreditCardTransactionDownPayment1(c *gin.Context,
 	}
 
 	if payload.Status != "3dVerify" {
-		return fmt.Errorf("request failed: [%s]", payload)
+		// return fmt.Errorf("request failed: [%s]", payload)
+		return fmt.Errorf("request failed: [%s]", fmt.Sprintf("Status: %s, Message: %s", payload.Status, payload.Message))
 	}
 
 	result, ok := payload.Result.(string)
